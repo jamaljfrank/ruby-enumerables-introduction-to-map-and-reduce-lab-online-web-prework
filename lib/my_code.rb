451a8141658b16dcaf9d1cfd0a4cc1064438d88d
Jamal Frank <jamaljfrank@gmail.com>
2019-07-16 03:15:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,14 +34,7 @@ sqrt_array
 end
 
 def reduce_to_total(source_array, starting_point=0)
-  total = starting_point
-  i = 0
-  while i < source_array.length do
-    total += source_array[i]
-    i += 1
-  end
-  total
-end
+ 
 def reduce_to_all_true(source_array)
   
     
