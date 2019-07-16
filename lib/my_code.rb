ffa6dda3973e4ec0ef3d02d266407aab44298500
Jamal Frank <jamaljfrank@gmail.com>
2019-07-16 02:45:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,8 +42,8 @@ def reduce_to_total(source_array, starting_point=0)
   end
   total
 end
-reduce_to_all_true(source_array)
-reduce_to_any_true(source_array)
+def reduce_to_all_true(source_array)
+def reduce_to_any_true(source_array)
   
 
 
