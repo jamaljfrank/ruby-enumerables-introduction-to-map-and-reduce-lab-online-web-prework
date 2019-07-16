10828f50cba012595ab45b5f565e2eb7213ec585
Jamal Frank <jamaljfrank@gmail.com>
2019-07-16 02:46:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,9 @@ def reduce_to_total(source_array, starting_point=0)
   total
 end
 def reduce_to_all_true(source_array)
-  
+  i = 0 
+  while i < source_array.length do
+    
 def reduce_to_any_true(source_array)
   
 
